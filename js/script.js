96e1bcd0d30876d35e2790d04cbc479fb6339ac4
Felipe Diaz <luisfdcf92@gmail.com>
2025-09-23 12:00:08 +0200

    fix bug in hamburger menu

--- js/script.js
+++ js/script.js
@@ -28,5 +28,6 @@ const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
 hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
   navLinks.classList.toggle('active');
 });
